6e58df1edf891722d76d0091c083f7de26076a9e
abedshamia <ab.shamia96@gmail.com>
2022-06-29 14:51:48 +0300

    Add Static Social Divider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import './App.css'
-import { Aboutus } from './components/Aboutus/Aboutus'
+import { Aboutus } from './components/AboutUs/Aboutus'
+import 'antd/dist/antd.min.css'
+import { SocialImpact } from './components/SocialImpact/SocialImpact'
 
 function App() {
   return (
     <div className="App">
       <h1>Lazzaro</h1>
       <Aboutus />
+      <SocialImpact />
     </div>
   )
 }
---
+++ src/components/SocialImpact/SocialImpact.tsx
@@ -0,0 +1,67 @@
+import React, { ReactElement } from 'react'
+import styled from 'styled-components'
+import { Typography } from 'antd'
+import { ImpactPart } from './ImpactPart/ImpactPart'
+
+const { Title } = Typography
+
+const numbers: object[] = [
+  {
+    key: '1',
+    num: 265,
+    text: 'Volunteers'
+  },
+  {
+    key: '2',
+    num: 120,
+    text: 'Candidates'
+  },
+  {
+    key: '3',
+    num: 1252,
+    text: 'Accounts raised'
+  },
+  {
+    key: '4',
+    num: 5015,
+    text: 'People supported'
+  },
+]
+export function SocialImpact(): ReactElement {
+  return (
+
+    <SocialImpactSection>
+      <SectionTitle level={1}>Social Impact </SectionTitle>
+      <ImpactNumbers>
+        {numbers.map((number: any) => (
+          <ImpactPart {...number} />
+        ))}
+      </ImpactNumbers>
+    </SocialImpactSection>
+  )
+}
+
+const SocialImpactSection = styled.section`
+width: 100%;
+height: 100%;
+background-color: #5CB780;
+display: flex;
+flex-direction: column;
+align-items: center;
+gap: 4.2rem;
+`
+
+const SectionTitle = styled(Title)`
+color: #fff !important;
+margin-top: 3.8rem;
+font-size: 3.25rem !important;
+`
+
+const ImpactNumbers = styled.div`
+display: flex;
+justify-content: space-between;
+width: 100%;
+padding: 0 10.2rem;
+padding-bottom: 4.2rem;
+margin-top: 1.2rem;
+`
