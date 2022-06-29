3c0f9ca0e235aef9ea6ae98cbc074e29a994b7de
abedshamia <ab.shamia96@gmail.com>
2022-06-29 17:55:18 +0300

    Add Subscribe Divider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import './App.css'
-import { Aboutus } from './components/Aboutus/Aboutus'
+// import { Aboutus } from './components/AboutUs/Aboutus'
+import 'antd/dist/antd.min.css'
+import { SubscribeDivider } from './components/SubscribeDivider/SubscribeDivider'
 
 function App() {
   return (
     <div className="App">
       <h1>Lazzaro</h1>
-      <Aboutus />
+      {/* <Aboutus /> */}
+      <SubscribeDivider />
     </div>
   )
 }
