09add4fbf191bd12d7578fd4f2cc3dcd1db6f9ab
abedshamia <ab.shamia96@gmail.com>
2022-07-05 12:50:25 +0300

    remove link

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,4 +1,4 @@
-export { default as Button } from './Button'
 export { default as Card } from './Card'
 export { default as ReadMore } from './ReadMore'
 export { default as SectionTitle } from './SectionTitle'
+export { default as Button } from './Button'
