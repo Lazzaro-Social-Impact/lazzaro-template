93e3c24941767c6f91e35195fe3c91734989ec5d
Amjad Yahia <amjadyahia.dev@gmail.com>
2022-07-06 17:36:06 +0300

    add css rule types

---
+++ src/components/common/CSSRuleTypes.ts
@@ -0,0 +1,8 @@
+type TTextAlign = 'left' | 'center' | 'right' | 'justify' | 'initial' | 'inherit';
+type TDecoration = 'none' | 'underline' | 'overline' | 'line-through' | 'initial' | 'inherit';
+type TTransform = 'none' | 'capitalize' | 'uppercase' | 'lowercase' | 'initial' | 'inherit';
+type TBgColor = 'transparent' | 'initial' | 'inherit' | string;
+
+export type {
+  TTextAlign, TDecoration, TTransform, TBgColor
+}
