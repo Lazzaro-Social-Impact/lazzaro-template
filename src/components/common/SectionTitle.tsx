fc3240b9135c0e3d97418faa1f5b1e80d4de10ed
Amjad Yahia <amjadyahia.dev@gmail.com>
2022-07-06 17:54:37 +0300

    add terms and condition

--- src/components/common/SectionTitle.tsx
+++ src/components/common/SectionTitle.tsx
@@ -5,6 +5,8 @@ interface IProps {
     padding?: number;
     marginTop?: number;
     marginBottom?: number;
+    color?: string;
+    textAlign?: 'center' | 'left' | 'right' | 'justify' | 'inherit' | 'initial';
 }
 const SectionTitle = styled.p<IProps>`
     margin-bottom: 2.4rem;
@@ -13,12 +15,17 @@ const SectionTitle = styled.p<IProps>`
     font-weight: bold;
     font-size: ${({ fontSize }) => `${fontSize}rem}`};
     margin-bottom: ${({ marginBottom }) => marginBottom}rem;
+    color: ${({ color }) => color};
+    text-align: ${({ textAlign }) => textAlign};
 `
 
 SectionTitle.defaultProps = {
   fontSize: 3.8,
   padding: 4.1,
-  marginTop: 4.2,
+  marginTop: 0,
+  marginBottom: 0,
+  color: 'initial',
+  textAlign: 'initial',
 }
 
 export default SectionTitle
