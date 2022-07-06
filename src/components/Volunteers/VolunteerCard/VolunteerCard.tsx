8634c5080415cff80b0f8e2ba73667e8020faf53
Amjad Yahia <amjadyahia.dev@gmail.com>
2022-07-06 09:32:48 +0300

    refactored volunterCard to use Custom Card

--- src/components/Volunteers/VolunteerCard/VolunteerCard.tsx
+++ src/components/Volunteers/VolunteerCard/VolunteerCard.tsx
@@ -1,70 +1,63 @@
 import { LinkedinFilled } from '@ant-design/icons'
 import React, { ReactElement } from 'react'
 import styled from 'styled-components'
+import { Card } from '../../common'
+import { useTheme } from '../../../app/context/theme-context'
 
 interface VolunteerCardProps {
-    name: string,
-    position: string,
-    image: {
-        src: string,
-        alt: string
-    }
-
+  name: string;
+  position: string;
+  image: {
+    src: string;
+    alt: string;
+  };
 }
 
-export function VolunteerCard({ name, position, image }: VolunteerCardProps)
-: ReactElement<VolunteerCardProps> {
+export function VolunteerCard({
+  name,
+  position,
+  image,
+}: VolunteerCardProps): ReactElement<VolunteerCardProps> {
+  const color = useTheme()
   return (
-    <Volunteer>
+    <Card mode="column" smMode="column" pb={1.5} textAlign="center">
       <VolunteerImage src={image.src} alt={image.alt} />
       <VolunteerName>{name}</VolunteerName>
-      <VolunteerPosition>{position}</VolunteerPosition>
-      <LinkedinFilled />
-    </Volunteer>
+      <VolunteerPosition color={color}>{position}</VolunteerPosition>
+      <LinkedinIcon />
+    </Card>
   )
 }
 
-const Volunteer = styled.div`
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    align-items: center;
-    padding-bottom: 1.8rem;
-    background-color: #fff;
-    box-shadow: 0px 4px 25px rgba(0, 0, 0, 0.25);
-    cursor: pointer;
-    transition: all 0.3s ease-in-out;
-
-    &:hover {
-        box-shadow: 0px 4px 25px rgba(0, 0, 0, 0.45);
-    }
-
-    svg {
-      font-size: 1.8rem;
-    }
-
-`
-
 const VolunteerImage = styled.img`
-    width: 378px;
-    height: 180px;
-    object-fit:cover;
-    object-position: center;
+  width: 378px;
+  height: 180px;
+  object-fit: cover;
+  object-position: center;
 
-    @media screen and (max-width: 768px) {
-        width: 300px;    
-        height: auto;
-    }
+  @media screen and (max-width: 768px) {
+    width: 100% !important;
+    height: auto;
+  }
 `
 const VolunteerName = styled.p`
-font-size: 1.1rem;
-margin-top: 1.2rem;
-margin-bottom: 0;
+  font-size: 1.1rem;
+  margin-top: 1.2rem;
+  margin-bottom: 0;
 `
-const VolunteerPosition = styled.p`
-  color: #5CB780;
-padding-bottom: 1.8rem;
-margin-top: 0.4rem;
-font-size: 1rem;
-font-weight: 300;
+const VolunteerPosition = styled.p<{ color: string }>`
+  color: ${({ color }) => color};
+  padding-bottom: 1.8rem;
+  margin-top: 0.4rem;
+  font-size: 1rem;
+  font-weight: 300;
+`
+
+const LinkedinIcon = styled(LinkedinFilled)`
+  font-size: 2rem;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+  &:hover {
+    color: #5cb780;
+  }
 `
