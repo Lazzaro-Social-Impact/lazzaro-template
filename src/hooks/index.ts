a1954b4ce5602aea63a7004b45c767fe6e5e8756
Amjad Yahia <amjadyahia.dev@gmail.com>
2022-07-26 16:41:37 +0300

    add index file for hooks folder

---
+++ src/hooks/index.ts
@@ -0,0 +1,5 @@
+export { default as useDependant } from './useDependant'
+export { default as useObserver } from './useObserver'
+export { default as usePostData } from './usePostData'
+export { default as useAppSelector } from './useAppSelector'
+export { default as useAppDispatch } from './useAppDispatch'
--- src/queryclient/index.tsx
+++ src/queryclient/index.tsx
@@ -1,8 +1,8 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import React from 'react'
+import { ReactNode } from 'react'
 
 const queryClient = new QueryClient()
 
-export default function QueryProvider({ children }: { children: React.ReactNode }) {
+export default function QueryProvider({ children }: { children: ReactNode }) {
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 }
