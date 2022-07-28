c1b3bd509d93f88b5dbd939f04e5d8277a8cc14b
Amjad Yahia <amjadyahia.dev@gmail.com>
2022-07-28 10:11:27 +0300

    made projects section carousel

--- src/components/common/ReadMore.tsx
+++ src/components/common/ReadMore.tsx
@@ -12,7 +12,7 @@ const ReadMore = styled.a<IProps>`
   text-decoration: underline;
   cursor: pointer;
   transition: all 0.2s ease-in-out;
-  color: ${({ color }) => color};
+  color: ${({ color, theme }) => color || theme.primary};
 
   &:hover {
     color: ${({ hoverColor }) => hoverColor};
@@ -23,7 +23,6 @@ const ReadMore = styled.a<IProps>`
 ReadMore.defaultProps = {
   fontSize: 'inherit',
   hoverColor: 'white',
-  color: 'var(--primary-color)',
 }
 
 export default ReadMore
