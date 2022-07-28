7a3df410d039c15a1dd83eeb2a003dcbd7a58486
abedshamia <ab.shamia96@gmail.com>
2022-07-28 10:22:33 +0300

    Validate and use react hook form for become a member form

--- src/components/Forms/BecomeMemberForm.tsx
+++ src/components/Forms/BecomeMemberForm.tsx
@@ -1,11 +1,43 @@
-import { Form, Input, Radio } from 'antd'
+import { Radio } from 'antd'
 import React, { ReactElement } from 'react'
 import styled from 'styled-components'
-import { Button } from '../common'
+import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
 import Footer from '../Footer/Footer'
 import Navbar from '../Navbar/Navbar'
 
+type memberSubmitForm = {
+  firstName: string
+  lastName: string
+  user_email: string
+  home_address: string
+  birthDate: string
+  nif: number
+  terms: boolean
+  membership: boolean
+  phone: string
+}
+const memberSchema = yup.object().shape({
+  firstName: yup.string().required('First name is required'),
+  lastName: yup.string().required('Last name is required'),
+  user_email: yup.string().required('Email is required'),
+  home_address: yup.string().required('Address is required'),
+  birthDate: yup.string().required('Birth of Date is required'),
+  nif: yup.number().required('ID is required').typeError('ID must be a number'),
+  terms: yup.boolean().typeError('You must accept the terms and conditions'),
+  membership: yup.boolean().typeError('You must accept the membership'),
+  phone: yup.string().required('Phone is required')
+}).required()
+
 export function BecomeMemberForm(): ReactElement {
+  const { register, handleSubmit, formState: { errors } } = useForm<memberSubmitForm>({
+    resolver: yupResolver(memberSchema),
+  })
+
+  const onSubmit = async (data: memberSubmitForm) => {
+    console.log(data)
+  }
   return (
     <>
       <Navbar />
@@ -16,7 +48,10 @@ export function BecomeMemberForm(): ReactElement {
             alt=""
           />
         </ImageContainer>
-        <Form layout="vertical" style={{ padding: '0 10.2rem' }}>
+        <form
+          style={{ padding: '0 10.2rem' }}
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <FormTitle>
             Membership registration
           </FormTitle>
@@ -25,83 +60,146 @@ export function BecomeMemberForm(): ReactElement {
             membership, we need some information from you.
           </FormSubtitle>
           <FormRow>
-            <Input
-              placeholder="Name"
-              size="large"
-              style={{ width: '100%' }}
-            />
-            <Input
-              placeholder="Surname"
-              size="large"
-              style={{ width: '100%' }}
-            />
+            <CustomInputDiv>
+              <CustomInput
+                placeholder="Name"
+                style={{ width: '100%' }}
+                {...register('firstName')}
+              />
+              <p>
+                {errors.firstName?.message}
+              </p>
+            </CustomInputDiv>
+            <CustomInputDiv>
+
+              <CustomInput
+                placeholder="Surname"
+                style={{ width: '100%' }}
+                {...register('lastName')}
+              />
+              <p>
+                {errors.lastName?.message}
+              </p>
+            </CustomInputDiv>
+
           </FormRow>
           <FormRow>
-            <Input
-              placeholder="DNI/NIF/Passport"
-              size="large"
-              style={{ width: '100%' }}
-            />
-            <Input
-              placeholder="Phone"
-              size="large"
-              style={{ width: '100%' }}
-            />
+            <CustomInputDiv>
+
+              <CustomInput
+                placeholder="DNI/NIF/Passport"
+                style={{ width: '100%' }}
+                {...register('nif')}
+              />
+              <p>
+
+                {errors.nif?.message}
+              </p>
+            </CustomInputDiv>
+            <CustomInputDiv>
+
+              <CustomInput
+                placeholder="Phone"
+                style={{ width: '100%' }}
+                {...register('phone')}
+              />
+              <p>
+
+                {errors.phone?.message}
+              </p>
+            </CustomInputDiv>
           </FormRow>
           <FormRow>
-            <Input
-              placeholder="Email"
-              size="large"
-              style={{ width: '100%' }}
-            />
-            <Input
-              placeholder="Date of birth"
-              size="large"
-              style={{ width: '100%' }}
-            />
+            <CustomInputDiv>
+
+              <CustomInput
+                placeholder="Email"
+                style={{ width: '100%' }}
+                {...register('user_email')}
+              />
+              <p>
+
+                {errors.user_email?.message}
+              </p>
+
+            </CustomInputDiv>
+
+            <CustomInputDiv>
+
+              <CustomInput
+                placeholder="Date of birth"
+                style={{ width: '100%' }}
+                {...register('birthDate')}
+              />
+              <p>
+
+                {errors.birthDate?.message}
+              </p>
+            </CustomInputDiv>
+
           </FormRow>
-          <Input
+          <CustomInput
             placeholder="Address (street, city and postal code)"
-            size="large"
             style={{ width: '100%', marginTop: '1.2rem' }}
+            {...register('home_address')}
           />
+          <p>
+            {errors.home_address?.message}
+          </p>
+
           <RadioQuestion>
             I have read and accepted the NGOs privacy policy.
           </RadioQuestion>
-          <CustomRadio
-            style={{ marginTop: '1.2rem' }}
-            name="privacy"
+          <CustomRadioGroup
+            {...register('terms')}
           >
-            I accept
-          </CustomRadio>
-          <CustomRadio
-            name="privacy"
-          >
-            I dont accept (in this case, we will not be able to process your membership)
-          </CustomRadio>
+
+            <CustomRadio
+              style={{ marginTop: '1.2rem' }}
+              value
+            >
+              I accept
+            </CustomRadio>
+            <CustomRadio
+              value={false}
+            >
+              I dont accept (in this case, we will not be able to process your membership)
+            </CustomRadio>
+          </CustomRadioGroup>
+          <p>
+            {errors.terms?.message}
+          </p>
+
           <RadioQuestion>
             Would you like us to process your registration as a member of the NGO?
           </RadioQuestion>
-          <CustomRadio
-            name="member"
+          <CustomRadioGroup
+            {...register('membership')}
           >
-            Yes
-          </CustomRadio>
-          <CustomRadio
-            name="member"
-          >
-            No
-          </CustomRadio>
+            <CustomRadio
+              value
+            >
+              Yes
+            </CustomRadio>
+            <CustomRadio
+              value={false}
+            >
+              No
+            </CustomRadio>
+          </CustomRadioGroup>
+          <p>
+
+            {errors.membership?.message}
+          </p>
+
           <Center>
-            <Button
-              px="2.4rem"
-              py="0.8rem"
-              bgColor="green"
+            <button
+              type="submit"
             >
               Submit
-            </Button>
+            </button>
           </Center>
-        </Form>
+        </form>
       </Container>
       <Footer />
     </>
@@ -153,8 +251,48 @@ const RadioQuestion = styled.p`
 color: #8c8c8c;
 letter-spacing: 1.2px;
 margin-top: 1.8rem;
+font-size: 1rem;
 `
+const CustomInput = styled.input`
+    -webkit-text-size-adjust: 100%;
+    -webkit-tap-highlight-color: rgba(0,0,0,0);
+    align-self: flex-start;
+    box-shadow: ${({ theme }) => theme.primary};
+    box-sizing: border-box !important;
+    font-family: inherit;
+    overflow: visible;
+    margin: 0;
+    font-variant: tabular-nums;
+    list-style: none;
+    font-feature-settings: "tnum";
+    position: relative;
+    display: inline-block;
+    min-width: 0;
+    padding: 9.5px 11px;
+    color: rgba(0,0,0,.85);
+    font-size: 14px;
+    line-height: 1.5715;
+    background-color: #fff;
+    background-image: none;
+    border: 1px solid #d9d9d9;
+    border-radius: 2px;
+    transition: all .3s;
+    -webkit-appearance: none;
+    touch-action: manipulation;
+    text-overflow: ellipsis;
+    width: 100%;
 
+    &:focus {
+        outline: none;
+        box-shadow: ${({ theme }) => theme.primary} 0 0 0 2px;
+    }
+
+    & + p {
+  color: red;
+  margin-bottom: 0;
+  margin-top: 0.4rem ;
+    }
+`
 const CustomRadio = styled(Radio)`
 display: block;
   span {
@@ -167,4 +305,19 @@ display: block;
   .ant-radio-inner::after {
     background-color: green !important;
   }
+`
+
+const CustomInputDiv = styled.div`
+display: flex;
+flex-direction: column;
+gap: 0;
+width: 100%;
+
+`
+const CustomRadioGroup = styled(Radio.Group)`
+ & + p {
+  color: red;
+  margin-top: 0.4rem ;
+
+ }
 `
