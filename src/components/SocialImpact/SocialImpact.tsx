58798e35b937a8cee211a5625d1c578a92a4c20c
abedshamia <ab.shamia96@gmail.com>
2022-08-04 13:04:01 +0300

    Integrate-Social-Impact-Section

--- src/components/SocialImpact/SocialImpact.tsx
+++ src/components/SocialImpact/SocialImpact.tsx
@@ -2,43 +2,24 @@ import React, { ReactElement } from 'react'
 import { useTheme } from 'styled-components'
 import ImpactPart from './ImpactPart/ImpactPart'
 import { Flex, SectionTitle } from '../common'
+import { useAppSelector } from '../../hooks'
 
 interface INumber {
-  key: string
-  num: number
-  text: string
+  name: string
+  amount: number | string
+  id: string
 }
-const numbers: INumber[] = [
-  {
-    key: '1',
-    num: 265,
-    text: 'Volunteers'
-  },
-  {
-    key: '2',
-    num: 120,
-    text: 'Candidates'
-  },
-  {
-    key: '3',
-    num: 1252,
-    text: 'Accounts raised'
-  },
-  {
-    key: '4',
-    num: 5015,
-    text: 'People supported'
-  },
-]
+
 export default function SocialImpact(): ReactElement {
   const { primary } = useTheme()
+  const impact = useAppSelector((state) => state.ong.ongConfig.impactData)
   return (
 
     <Flex id="impact" direction="column" bgColor={primary} gap={2.5} px={5}>
       <SectionTitle color="white" fontSize={2.8} marginTop={3}>Social Impact </SectionTitle>
 
       <Flex wrap="nowrap" mt={1.2} mb={4}>
-        {numbers.map((number: INumber) => (<ImpactPart {...number} />))}
+        {impact?.map((section: INumber) => (<ImpactPart {...section} key={section.id} />))}
       </Flex>
     </Flex>
   )
