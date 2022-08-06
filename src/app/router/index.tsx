7a46f5519c03e7958873e30f28d022a12e786f84
Amjad Yahia <amjadyahia.dev@gmail.com>
2022-08-06 14:12:40 +0300

    implement ongChoices

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import { BecomeMemberForm } from '../../components/Forms/BecomeMemberForm'
 import { ContactusForm } from '../../components/Forms/ContactusForm'
+import { useAppSelector } from '../../hooks'
 import {
   Aboutus,
   BecomeVolunteerForm,
@@ -15,20 +16,25 @@ import {
 } from '../../views'
 
 export default function AllRoute() {
+  const {
+    causes, events, market, courses, volunteers, partners, donations,
+  } = useAppSelector(({ ong }) => ong.ongConfig?.features) || {}
+
   return (
     <>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/about" element={<Aboutus />} />
-        <Route path="/events/:id" element={<SingleEvent />} />
-        <Route path="/courses/:id" element={<SingleEvent />} />
+        {events && <Route path="/events/:id" element={<SingleEvent />} />}
+        {courses && <Route path="/courses/:id" element={<SingleEvent />} />}
+        {causes && <Route path="/projects/:id" element={<ProjectDetails />} />}
+        {market && <Route path="/products/:id" element={<SingleProduct />} />}
+        {market && <Route path="/shop" element={<Shop />} />}
+        {partners && <Route path="/partners" element={<BecomeMemberForm />} />}
+        {volunteers && <Route path="/volunteers" element={<BecomeVolunteerForm />} />}
+        {donations && <Route path="/donate" element={<Donate />} />}
+
         <Route path="/terms_and_conditions" element={<TermsAndConditions />} />
-        <Route path="/projects/:id" element={<ProjectDetails />} />
-        <Route path="/products/:id" element={<SingleProduct />} />
-        <Route path="/partners" element={<BecomeMemberForm />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/volunteers" element={<BecomeVolunteerForm />} />
-        <Route path="/donate" element={<Donate />} />
         <Route path="/contact" element={<ContactusForm />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
