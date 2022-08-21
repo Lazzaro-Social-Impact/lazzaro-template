c749e06e86975fa2c38f2e7b5648c83755dbbcef
= <=>
2022-08-21 13:25:12 +0300

    moved store file to redux folder

--- src/hooks/useAppSelector.ts
+++ src/hooks/useAppSelector.ts
@@ -1,5 +1,5 @@
 import { TypedUseSelectorHook, useSelector } from 'react-redux'
-import type { RootState, } from '../app/store'
+import type { RootState, } from '../redux/store'
 
 const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
 
