17e54aa88810b3179b4f77bfe3830e48e5f5efc4
Amjad Yahia Robeen Hassan <amjadyahia.dev@gmail.com>
2022-08-24 19:39:11 +0300

    Delete @types/react-18next directory

--- @types/react-18next/index.d.ts
+++
@@ -1,9 +0,0 @@
-import 'react-i18next'
-import { resources, defaultNS } from '../../src/i18n/config'
-
-declare module 'react-i18next' {
-    interface CustomTypeOptions {
-      defaultNS: typeof defaultNS;
-      resources: typeof resources['en'];
-    }
-  }
