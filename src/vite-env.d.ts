14b9f5da88750035e28aac2bcd7dfd66405a9160
= <=>
2022-08-25 13:37:31 +0300

    remove google map and vite evn variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_ENV: 'development' | 'production' | 'staging';
   readonly VITE_MAPBOX_TOKEN: string;
-  readonly VITE_GOOGLE_MAP_KEY: string;
 }
 
 interface ImportMeta {
