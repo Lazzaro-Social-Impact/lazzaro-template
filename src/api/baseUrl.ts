18e5222b046a9ecce4c790fc50ce9c05a7d6ce90
abedshamia <ab.shamia96@gmail.com>
2022-09-15 13:49:54 +0300

    get hostname if staging or prod

--- src/api/baseUrl.ts
+++ src/api/baseUrl.ts
@@ -5,7 +5,7 @@ interface IEnvironments {
 }
 
 const ENV_BASE_URL: IEnvironments = {
-  development: 'http://localhost:8080',
+  development: 'https://elbpre.backend.lazzaro.io:8080',
   production: 'https://elb.backend.lazzaro.io:8080',
   staging: 'https://elbpre.backend.lazzaro.io:8080',
 }
