0e607afa9c82cc714bfdb0a721b9c4672197feef
= <=>
2022-12-07 11:08:55 +0200

    fix ProductCard main image being cached

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,55 +1,53 @@
-import type { ReactElement, SyntheticEvent } from 'react'
-import styled from 'styled-components'
-import { useNavigate } from 'react-router-dom'
-import { BookmarkIcon } from '../Icons'
-import { Flex, Text } from '../common'
-import { useAppSelector } from '../../hooks'
-import { IProductCard } from '../../types/interfaces'
-import { LazyImageComponent } from '../common/LazyImage'
+import type { ReactElement, SyntheticEvent } from 'react';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import { BookmarkIcon } from '../Icons';
+import { Flex, Text } from '../common';
+import { useAppSelector } from '../../hooks';
+import { IProductCard } from '../../types/interfaces';
+import { LazyImageComponent } from '../common/LazyImage';
 
-export function ProductCard({
-  id, title, price, default_img: img, discount,
-}: IProductCard): ReactElement {
-  const currency = useAppSelector((state) => state.ong?.ongConfig?.platformConfig?.currency_symbol)
-  const navigate = useNavigate()
-  const navigateTo = (path: `/products/${string}`) => () => navigate(path)
+export function ProductCard({ id, title, price, default_img: img, discount }: IProductCard): ReactElement {
+  const currency = useAppSelector((state) => state.ong?.ongConfig?.platformConfig?.currency_symbol);
+  const navigate = useNavigate();
+  const navigateTo = (path: `/products/${string}`) => () => navigate(path);
 
-  const handleBrokenImage = (e:SyntheticEvent<HTMLImageElement>) => {
+  const handleBrokenImage = (e: SyntheticEvent<HTMLImageElement>) => {
     const fallBackImage = 'https://i.pinimg.com/originals/9b/96/92/9b9692c9f0db9f6276e6bd29a98c25e0.png';
-    (e.target as HTMLImageElement).src = fallBackImage
-  }
+    (e.target as HTMLImageElement).src = fallBackImage;
+  };
 
   return (
     <SingleProductCard onClick={navigateTo(`/products/${id}`)}>
       <ProductImage>
         <LazyImageComponent
-          height="230px"
-          width="100%"
-          src={img}
+          height='230px'
+          width='100%'
+          src={`${img}?${Date.now()}`}
           alt={title}
-          effect="blur"
+          effect='blur'
           onError={handleBrokenImage}
           placeholderSrc={img}
         />
       </ProductImage>
 
-      <Flex wrap="nowrap" py={0.4} px={0.8} align="flex-start">
-        <P textAlign="left" flex={1}>
+      <Flex wrap='nowrap' py={0.4} px={0.8} align='flex-start'>
+        <P textAlign='left' flex={1}>
           {title}
         </P>
-        <P textAlign="end">
+        <P textAlign='end'>
           {price.toFixed(2)} {currency}
         </P>
       </Flex>
-      {!!discount && <BookmarkIcon position="absolute" top={0} right={0} text={`-${discount}`} />}
+      {!!discount && <BookmarkIcon position='absolute' top={0} right={0} text={`-${discount}`} />}
     </SingleProductCard>
-  )
+  );
 }
 
 const ProductImage = styled.div`
   width: 100%;
   height: 230px;
-`
+`;
 const SingleProductCard = styled.div`
   width: 300px;
   height: 340px;
@@ -59,9 +57,9 @@ const SingleProductCard = styled.div`
   &:hover {
     box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
   }
-`
+`;
 
 const P = styled(Text)`
   font-weight: bold;
   color: #777777;
-`
+`;
--- src/components/StripeContainer/index.tsx
+++ src/components/StripeContainer/index.tsx
@@ -1,29 +1,32 @@
-import { Elements } from '@stripe/react-stripe-js'
-import { loadStripe } from '@stripe/stripe-js'
-import { useParams } from 'react-router-dom'
-import CheckoutForm from '../Forms/CheckoutForm'
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import { useParams } from 'react-router-dom';
+import CheckoutForm from '../Forms/CheckoutForm';
 
-const stripePromise = loadStripe('pk_live_3bEHfZGESiO6objCXzJxzYmE')
+const stripePromise =
+  import.meta.env.mode === 'dev'
+    ? loadStripe('pk_test_b8602Fd2SVOySbs7AngcMs68')
+    : loadStripe('pk_live_3bEHfZGESiO6objCXzJxzYmE');
 
 type TClientSecret = {
-    secret: string
-}
+  secret: string;
+};
 function StripeContainer({ secret }: TClientSecret) {
-  const { secret: clientSecretParams, } = useParams()
+  const { secret: clientSecretParams } = useParams();
 
   const options = {
     // clientSecret coming from the form or the param url
     clientSecret: secret || clientSecretParams,
-  }
+  };
   return (
     <div>
       {(secret || clientSecretParams) && (
-      <Elements stripe={stripePromise} options={options}>
-        <CheckoutForm secret={secret || clientSecretParams} />
-      </Elements>
+        <Elements stripe={stripePromise} options={options}>
+          <CheckoutForm secret={secret || clientSecretParams} />
+        </Elements>
       )}
     </div>
-  )
+  );
 }
 
-export default StripeContainer
+export default StripeContainer;
