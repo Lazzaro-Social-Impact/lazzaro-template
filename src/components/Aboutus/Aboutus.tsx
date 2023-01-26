746ae614333f4817e5ad68469cb638a46e3d1f20
Amjad Yahia <amjadyahia.dev@gmail.com>
2023-01-26 17:09:02 +0200

    make about us image height fixed

--- src/components/Aboutus/Aboutus.tsx
+++ src/components/Aboutus/Aboutus.tsx
@@ -1,53 +1,61 @@
-import HtmlParser from 'html-react-parser'
-import { useTranslation } from 'react-i18next'
-import styled from 'styled-components'
-import {
-  Box, Flex, Link, SectionTitle
-} from '../common'
-import { useAppSelector } from '../../hooks'
-import { LazyImageComponent } from '../common/LazyImage'
+import HtmlParser from 'html-react-parser';
+import { useTranslation } from 'react-i18next';
+import styled from 'styled-components';
+import { Box, Flex, Link, SectionTitle } from '../common';
+import { useAppSelector } from '../../hooks';
+import { LazyImageComponent } from '../common/LazyImage';
 
 export default function AboutUs(): React.ReactElement {
   const {
-    description = '', img_url: imgUrl, title_description: titleDescription
-  } = useAppSelector(({ ong }) => ong.ongConfig?.description) || {}
-  const { t } = useTranslation()
+    description = '',
+    img_url: imgUrl,
+    title_description: titleDescription,
+  } = useAppSelector(({ ong }) => ong.ongConfig?.description) || {};
+  const { t } = useTranslation();
   return (
-    <CustomFlex id="about" align="stretch" mt={4.2} pl={4.1}>
-      <Flex direction="column" align="stretch" justify="stretch" textAlign="left" flex={1}>
+    <CustomFlex id='about' align='stretch' mt={4.2} pl={4.1}>
+      <Flex direction='column' align='stretch' justify='stretch' textAlign='left' flex={1}>
         <SectionTitle marginTop={0} marginBottom={0.6} padding={0.2} fontSize={2.2}>
           {titleDescription}
         </SectionTitle>
 
-        <Box fontSize="15px" pr={2.8} color="#777">
+        <Box fontSize='15px' pr={2.8} color='#777'>
           {description && description.length > 1020 ? (
             <>
-              <Box>
-                {HtmlParser(description.slice(0, 810))}...
-              </Box>
+              <Box>{HtmlParser(description.slice(0, 810))}...</Box>
 
-              <Link to="/about" align="flex-end" mt={1.8} underlined>
+              <Link to='/about' align='flex-end' mt={1.8} underlined>
                 {t('Read More')}
               </Link>
             </>
-          )
-            : description && description.length < 1020 ? HtmlParser(description) : ''}
+          ) : description && description.length < 1020 ? (
+            HtmlParser(description)
+          ) : (
+            ''
+          )}
         </Box>
-
       </Flex>
 
-      <Flex justify="flex-end" align="flex-start" flex={1}>
-        <LazyImageComponent
-          src={imgUrl}
-          alt="About us"
-          placeholderSrc={imgUrl}
-        />
+      <Flex justify='flex-end' align='flex-start' flex={1}>
+        <AboutUsImage src={imgUrl} alt='About us' placeholderSrc={imgUrl} />
       </Flex>
     </CustomFlex>
-  )
+  );
 }
 
 const CustomFlex = styled(Flex)`
+  .lazy-load-image-background {
+    width: 100%;
+  }
+  p,
+  strong,
+  span {
+    color: rgb(34, 34, 34) !important;
+    line-height: 20px !important;
+    font-size: 16px !important;
+    font-weight: 400 !important;
+  }
+
   @media screen and (max-width: 690px) {
     flex-direction: column;
     padding: 4.1rem;
@@ -55,14 +63,9 @@ const CustomFlex = styled(Flex)`
     P {
       font-size: 13px;
     }
-
   }
-  p,
-    strong,
-    span {
-      color: rgb(34, 34, 34) !important;
-      line-height: 20px !important;
-      font-size: 16px !important;
-      font-weight: 400 !important;
-    }
-`
+`;
+
+const AboutUsImage = styled(LazyImageComponent)`
+  max-height: 600px;
+`;
--- src/components/common/LazyImage.tsx
+++ src/components/common/LazyImage.tsx
@@ -1,11 +1,10 @@
-import { LazyLoadImage } from 'react-lazy-load-image-component'
-import styled from 'styled-components'
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import styled from 'styled-components';
 
 export const LazyImageComponent = styled(LazyLoadImage)`
-width: 100% !important;
-height: 100%;
-object-fit: cover;
-object-position: center;
-max-width: 100%;
-
-`
+  width: 100% !important;
+  height: 100%;
+  object-fit: cover;
+  object-position: center;
+  max-width: 100%;
+`;
