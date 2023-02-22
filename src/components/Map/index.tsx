c5fee303e5cdd94894b1e2ed0fbd23a950d4d0bc
Amjad Yahia <amjadyahia.dev@gmail.com>
2023-02-22 13:22:22 +0200

    fix mapbox token import

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,29 +1,25 @@
-import {
-  memo, useEffect, useRef, type FC
-} from 'react'
-import mapboxgl from 'mapbox-gl'
-import { Box } from '../common'
-import Skeleton from '../Skeleton'
+import { memo, useEffect, useRef, type FC } from 'react';
+import mapboxgl from 'mapbox-gl';
+import { Box } from '../common';
+import Skeleton from '../Skeleton';
 
 interface IProps {
-    lat: number;
-    lng: number;
-    height: number;
-    isLoading: boolean;
+  lat: number;
+  lng: number;
+  height: number;
+  isLoading: boolean;
 }
 
-const Map: FC<IProps> = ({
-  height, lat, lng, isLoading
-}) => {
-  const mapContainerRef = useRef<HTMLDivElement>(null)
-  const mapRef = useRef<mapboxgl.Map>()
-  mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN
+const Map: FC<IProps> = ({ height, lat, lng, isLoading }) => {
+  const mapContainerRef = useRef<HTMLDivElement>(null);
+  const mapRef = useRef<mapboxgl.Map>();
+  mapboxgl.accessToken = import.meta.env.REACT_APP_MAPBOX_TOKEN;
 
   useEffect(() => {
-    const mapAlreadyDefined = mapRef.current
-    const mapContainerNotDefined = !mapContainerRef.current
+    const mapAlreadyDefined = mapRef.current;
+    const mapContainerNotDefined = !mapContainerRef.current;
 
-    if (mapAlreadyDefined || mapContainerNotDefined) return
+    if (mapAlreadyDefined || mapContainerNotDefined) return;
 
     mapRef.current = new mapboxgl.Map({
       container: mapContainerRef.current,
@@ -31,11 +27,11 @@ const Map: FC<IProps> = ({
       center: [lng, lat],
       zoom: 12,
       attributionControl: false,
-    })
-  }, [lat, lng])
+    });
+  }, [lat, lng]);
 
-  if (isLoading) return <Skeleton width={100} height={height} number={1} px={0} mt={0} />
+  if (isLoading) return <Skeleton width={100} height={height} number={1} px={0} mt={0} />;
 
-  return <Box ref={mapContainerRef} height={height} />
-}
-export default memo(Map)
+  return <Box ref={mapContainerRef} height={height} />;
+};
+export default memo(Map);
